7a8632bd85fbfad4f15d224cf336a4c59e10891c
Devian <info@devian.tk>
2015-01-28 16:07:21 +0100

    setBorder

--- index.js
+++ index.js
@@ -3,21 +3,50 @@ const util = require('util');
 const events = require('events');
 const discoverer = require('./lib/discovery');
 
-
 const remotePort = 9992;
 
-var SmartView = module.exports = function(address) {
+var SmartView = module.exports = function(address, listeners) {
 
 	var self = this;
 
 	events.EventEmitter.call(this);
 
 	const connection = new net.Socket();
+	connection.setEncoding('utf8');
 
-	connection.on('message', function(data) {
-		console.log(data);
+	connection.on('data', function(data) {
+		var header = data.slice(0, data.indexOf(':'));
+
+		const body = {};
+		data.split('\n').slice(1, -2).forEach(function(line) {
+			const keyValue = line.split(': ');
+			
+			const key = camelize(keyValue[0]);
+			const value = (key == 'version') ? keyValue[1] : inferType(keyValue[1]);
+			
+			body[key] = value;
+		});
+
+		self.emit(data);
+		if (header.search('MONITOR') != -1)
+			self.emit('monitorInfo', header, body);
 	});
 
+	connection.on('close', function() {
+		self.emit('disconnect');
+	});
+
+	self.on('SMARTVIEW DEVICE', function(info) {
+		self.emit('deviceInfo', info);
+	});
+
+	self.on('NETWORK', function(settings) {
+		self.emit('networkSettings', settings);
+	});
+
+	for (var name in listeners) {
+		self.on(name, listeners[name]);
+	}
 
 
 
@@ -31,7 +60,6 @@ var SmartView = module.exports = function(address) {
 					connection.connect(remotePort, address);
 				}
 			} else {
-				console.log('invalid IPv4 address');
 				// todo
 				// emit error
 				if (!net.isIPv4(address)) address = undefined;
@@ -45,14 +73,44 @@ var SmartView = module.exports = function(address) {
 	this.connect = function() {
 		if (typeof this.address !== undefined) {
 			connection.destroy();
-			connection.connect(remotePort, self.address);
+			connection.connect(remotePort, self.address, function() {
+				self.emit('connected');
+			});
 		}
 	};
 
+	this.setBorder = function(monitorId, color) {
+		var command = monitorId + ':\n';
+		command += 'Border: ' + color + '\n\n';
+
+		const buf = new Buffer(command, 'utf8');
+
+		return connection.write(command);
+	}
+
 	this.address = address;
-	this.connect();
+	if (typeof listeners != 'undefined') self.connect();
 };
 
 util.inherits(SmartView, events.EventEmitter);
 
 discoverer(SmartView);
+
+function camelize(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
+  }).replace(/\s+/g, '');
+}
+
+function inferType(stringValue) {
+	if (!isNaN(stringValue))
+		var value = eval(stringValue);
+	else if (stringValue == 'false')
+		var value = false;
+	else if (stringValue == 'true')
+		var value = true;
+	else
+		var value = stringValue
+
+	return value;
+}
