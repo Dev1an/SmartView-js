c5efad5b8733eff30733af7311fd2fc54c1442f8
Devian <info@devian.tk>
2015-01-28 20:29:42 +0100

    closed socket behavior

--- index.js
+++ index.js
@@ -1,116 +1,55 @@
 const net = require('net');
 const util = require('util');
 const events = require('events');
-const discoverer = require('./lib/discovery');
+const Future = require('fibers/future');
 
-const remotePort = 9992;
-
-var SmartView = module.exports = function(address, listeners) {
-
-	var self = this;
-
-	events.EventEmitter.call(this);
-
-	const connection = new net.Socket();
-	connection.setEncoding('utf8');
-
-	connection.on('data', function(data) {
-		var header = data.slice(0, data.indexOf(':'));
-
-		const body = {};
-		data.split('\n').slice(1, -2).forEach(function(line) {
-			const keyValue = line.split(': ');
-			
-			const key = camelize(keyValue[0]);
-			const value = (key == 'version') ? keyValue[1] : inferType(keyValue[1]);
-			
-			body[key] = value;
-		});
-
-		self.emit(data);
-		if (header.search('MONITOR') != -1)
-			self.emit('monitorInfo', header, body);
-	});
-
-	connection.on('close', function() {
-		self.emit('disconnect');
-	});
-
-	self.on('SMARTVIEW DEVICE', function(info) {
-		self.emit('deviceInfo', info);
-	});
-
-	self.on('NETWORK', function(settings) {
-		self.emit('networkSettings', settings);
-	});
-
-	for (var name in listeners) {
-		self.on(name, listeners[name]);
-	}
+const makeDiscoverer = require('./lib/discovery');
+const SmartViewConnection = require('./lib/connection');
 
 
+const SmartView = module.exports = new events.EventEmitter();
 
+makeDiscoverer(SmartView);
 
-	Object.defineProperty(this, 'address', {
-		set: function(newAddress) {
-			if (net.isIPv4(newAddress)) {
-				address = newAddress;
-				if (connection.writable){
-					connection.destroy();
-					connection.connect(remotePort, address);
-				}
-			} else {
-				// todo
-				// emit error
-				if (!net.isIPv4(address)) address = undefined;
-			}
-		},
-		get: function() {
-			return address;
+SmartView.setBorder = function(address, monitor, color) {
+	new SmartViewConnection(address, {
+		connected: function() {
+			this.on('acknowledgement', function() {this.close()});
+			this.setBorder(monitor, color);
 		}
 	});
-
-	this.connect = function() {
-		if (typeof this.address !== undefined) {
-			connection.destroy();
-			connection.connect(remotePort, self.address, function() {
-				self.emit('connected');
-			});
-		}
-	};
-
-	this.setBorder = function(monitorId, color) {
-		var command = monitorId + ':\n';
-		command += 'Border: ' + color + '\n\n';
-
-		const buf = new Buffer(command, 'utf8');
-
-		return connection.write(command);
-	}
-
-	this.address = address;
-	if (typeof listeners != 'undefined') self.connect();
-};
-
-util.inherits(SmartView, events.EventEmitter);
-
-discoverer(SmartView);
-
-function camelize(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
-    return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
-  }).replace(/\s+/g, '');
 }
 
-function inferType(stringValue) {
-	if (!isNaN(stringValue))
-		var value = eval(stringValue);
-	else if (stringValue == 'false')
-		var value = false;
-	else if (stringValue == 'true')
-		var value = true;
-	else
-		var value = stringValue
+SmartView.getInfo = function(address, callback) {
+	var deviceInfo = new Future();
+	var monitorInfo = new Future();
+	var monitors = [];
+	var networkSettings = new Future();
 
-	return value;
+	Future.task(function() {
+		const connection = new SmartViewConnection(address, {
+			deviceInfo: function(info) {
+				deviceInfo.return(info);
+			},
+			monitorInfo: function(monitor, info) {
+				monitors.push(info);
+				info.id = monitor;
+
+				if (monitors.length == deviceInfo.wait().monitors) {
+					monitorInfo.return(monitors);
+				}
+			},
+			networkSettings: function(settings) {
+				networkSettings.return(settings);
+			}
+		});
+
+		var info = deviceInfo.wait();
+		info.monitors = monitorInfo.wait();
+		for (var key in networkSettings.wait()) {
+			info[key] = networkSettings.wait()[key];
+		}
+		connection.close();
+		callback(info);
+	}).detach();
 }
